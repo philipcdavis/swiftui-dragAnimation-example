c19019ef691c3bed0b0ede709f67b6a0b5beef49
Philip Davis <philipcdavis11@gmail.com>
2021-01-19 10:39:06 -0500

    Initial Commit

---
+++ DragProjection/DragProjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragProjectionApp.swift
+//  DragProjection
+//
+//  Created by Philip Davis on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct DragProjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
